2d504643b593044dd6d0b8728f5f51be1b8f2d97
Karen Perez <110954629+xkarimx@users.noreply.github.com>
2023-04-07 10:32:34 -0300

    Karen dev (#3)
    * navbar
    
    * fix

---
+++ src/components/H2/H2.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function H2({text}) {
+  return (
+    <h2>{text}</h2>
+  )
+}
---
+++ src/components/Image/Image.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Image({className,src}) {
+  return (
+    <img className={className} src={src}/>
+  )
+}
