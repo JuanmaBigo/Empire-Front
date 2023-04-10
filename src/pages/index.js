2a8a12b8cc51352c76121b5cd471fbe7bc714751
Karen <karen.perez.0395@gmail.com>
2023-04-10 12:53:45 -0300

    boton send de page contact funcionando

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,17 @@
 import React from 'react'
-
 import MainLayout from '../layouts/MainLayout/MainLayout'
-
-import { Navigate } from 'react-router-dom';
-
-import { createBrowserRouter } from "react-router-dom"
 import Welcome from './Welcome/Welcome';
 import Home from './Home/Home';
-
 import Login from './Login/Login';
 import Register from './Register/Register';
-
 import Museum from './Museum/Museum';
 import MuseumTour from './TourMuseum/TourMuseum';
 import Service from './Service/Service';
 import Vehicles from './Vehicles/Vehicles';
 import Contact from './Contact/Contact'
+import ContactUs from './ContactUs/ContactUs';
+
+import { createBrowserRouter } from "react-router-dom"
 
 
 //let token = localStorage.getItem('token')
@@ -33,6 +29,7 @@ export const router = createBrowserRouter([
             {path:'art&culture/museum-tour', element: <MuseumTour/>},
             {path:'services', element: <Service/>},
             {path:'services/contact', element: <Contact/>},
+            {path:'services/contact-us', element: <ContactUs />},
             {path:'vehicles', element: <Vehicles/>},
         ]
     } 
