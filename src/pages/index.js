e7aeca3bf94d9b1a72a43c288ed0f8dd2056b212
Karen <karen.perez.0395@gmail.com>
2023-04-11 18:41:05 -0300

    maquetado carrito terminado

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import Service from './Service/Service';
 import Vehicles from './Vehicles/Vehicles';
 import Contact from './Contact/Contact'
 import ContactUs from './ContactUs/ContactUs';
+import Carrito from './Carrito/Carrito';
 
 import { createBrowserRouter } from "react-router-dom"
 
@@ -31,6 +32,7 @@ export const router = createBrowserRouter([
             {path:'services/contact', element: <Contact/>},
             {path:'services/contact-us', element: <ContactUs />},
             {path:'vehicles', element: <Vehicles/>},
+            {path:'carrito', element: <Carrito />}
         ]
     } 
     
