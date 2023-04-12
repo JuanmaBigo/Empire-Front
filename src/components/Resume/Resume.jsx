4c84cdf52825ccef0c8ee31b514a824ca40aafbb
Karen Perez <110954629+xkarimx@users.noreply.github.com>
2023-04-12 17:37:13 -0300

    Resume (#18)
    * maquetado terminado
    
    * maquetado terminado
    
    * maquetado terminado

---
+++ src/components/Resume/Resume.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './resume.css'
+
+export default function Resume() {
+  return (
+    <div className='contain-resume'>
+        <div className='container-detail-car'>
+            <h1 className='color'>Title</h1>
+            <h3 className='color'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium eum nihil dolores minus qui, sapiente dolorum eligendi est ad velit minima reiciendis corporis deleniti aliquid laborum suscipit ratione delectus labore!</h3>
+            <img className='img' src='https://configuratormedia.lamborghini.com/renderservice/media/fast/H4sIAAAAAAAAAC2Vy6oltxWGd3fT7SYDN3YIJjOPDoGGLNW9anAINp7FA4NNBulBI5UupSrVTVJVnVMP4qfww3huCHkBjzzxxJC_NgHxf1tLS0taS9qqn369vd787dWHD1__84-__sWYX6qXt9vTcrvdXsD-8sPXP_32xe9v3v7w3_-bP3l7ex03L-YXcPn3yz_ZkRtF_aLMu9snMN1eY-DF8yU_Qt58CXmV3i7Nrin9NfLz1X_zn1e32zuY3l6r3W6ffrp5t3DPx7-3fl6mz_5WNKJJklSyTLWiKSSrmZB1UcpWci5qXgvZsLxtPkcwx52jMUwJnWPGhoRRytKckvx9lbCaivJ90xDnm2KcuJjLLgV8sqCnZCoNcTNW4wYElsNoRb5pwJk9Ie7MqoBJlEYQXxYTMuJejx3UB2hQ5dMAGMUZ4IrQA6MZc8AXHoHDMxswa5PN0QKB14H4sZcDJpz8PKHyruqu3RX8HO6_x74jwbtUGSD2TAF74iQJZKI3Eu0CbyGXvktIqC4fGQknBHIVTg1TRmI-kpCSiLx6hufJ4d_y6dKOp5jVzm3LGHC3LdzGDBB2g21pe3X1pD0AL7Cn1i9Iuz32VOYkuUrHBXCwSb4hglQ20xYYTGQkNbaZkJzna2j2ly46f1KkWjXZhJQ8Ku5I2a4YV2BYGwG4asSY3dbakhrt5CtSS1c9paSCrQ4GhE3XpEWLkFoMRcJJt_dOa8Ol_q6bnUirrcThaBN2B9hY-pa0CzVORftB9ynpIDbUQoelcOhFjrrrPVQGtsOuG-n7QelT3LW7a9Aos1FTbU4y3VVYM84xfyYz6RzlM57XsQTi3l69veA5mbCYRFMnHfw76RGps2M59dSFo7aRuiMkHSc7ha24EPOJkfUhx8Zs5KUQNHA_1C0NYmc478Gd7DmhYVxWXwMhlzAGXrQXbGYubAMqMGymHDzQDUh9OLtywbxzL4-UHG-LIQcGLUpy4sD2XDthe65d1pWRk7PC5XeKO-aAaT0rcpbXqJEbeMUiIKqMk5t9hcDO8-TpAHZcWXJB1R4uIaRtQW4bq06RO3y9ZjRyOY0lEEpct1EGjbRGzZtQ09jJidNoh0w2NPanfcbQsNVnQSi2TDcavWhwrhPfqzWlaeIa1ZhlnFZgcspltCQ5Gi18NAujpZ3QgNg0jhbZo9FiuMaFXTfOkaznsmQCmG0dybez8gkQUBQvhwrV88rW-QH4BAX2ITKVAnu2CgqcT1sGiPwpoSB8HQYgIpGgrhsUlEWkoEOKNyCYoVng3ck1UrBiyntAZa4AupxjyJ7MWQoD75_gP8QCRQhO1ToF9BVw4hXyDFNXI_kwb5dt2arLz3elxVDo8qEFXC7gEdvLI-q0R9wYTQ_H-IS_TNhnjasScDLXWsfZ4KWMRpW9oDjsOZaMi2kEA5xAUaPXhUwoInWnKO5q3TOKh0v2keLzkugJOFOc1y6GJAJzWNOeDqbQ6EgYGh3cq6Ogo72eiCPwSsEWQm40HXuCRqfw6aHoxN2IgFX5eSGi_UNNu_V4wNQUH7-Pm7Tzx2_5KGZvOjvZh8PK2D0mTcoeOmVNFx_xZWAP3C0df4x-Uw8tH5Xnj1_967v0m49ffUw_MpY98AnfuGjnKTx-w5_dNfHPr-DyPxXRGTA7BwAA.png?viewPort=780,320,insert' alt="car" />
+        </div>
+        <div className='container-detail-pay'>
+          <div className='div-detail-pay'>
+            <h2>RESUME</h2>
+          </div>
+          <div className='div-detail-pay'>
+            <h3>BASE <p>Lamborghini Revuelto</p></h3>
+            <h3>$500.000</h3>
+          </div>
+          <div className='div-detail-pay'>
+            <h3>COLOR <p>Arancio Borealis</p></h3>
+            <h3>$5.000</h3>
+          </div>
+          <div className='div-detail-pay'>
+            <h3>RIM SELECTION <p>Rims Aesir graphite grey</p></h3>
+            <h3>$7.500</h3>
+          </div>
+          <div className='div-detail-pay'>
+            <h3>TOTAL </h3>
+            <h3>$512.500</h3>
+          </div>
+          <div className='div-detail-pay'>
+            <h3>RESERVATION </h3>
+            <h3>$45.000</h3>
+          </div>
+          <button className='btn-add-car'>ADD TO CART</button>
+        </div>
+    </div>
+  )
+}
