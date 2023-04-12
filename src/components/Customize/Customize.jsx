51a0fd08d2708bd8af5c8fc29c0fa89ca4cf7c26
RominaGaite <gaiteromina@gmaul.com>
2023-04-12 17:57:20 -0300

    .

---
+++ src/components/Customize/Customize.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './customize.css'
+
+export default function Customize() {
+  return (
+    <div className='cont-customize'>
+      <div className='div-rim'>
+      <h2 className='title-rim'>Select Rim</h2>
+            <div className='section-rim'>
+              <img className='rim-1' src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQcAAADACAMAAAA+71YtAAAAA1BMVEXjABuGu5rPAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+BsYAAAF7hZJ0AAAAAElFTkSuQmCC'></img>
+            </div>
+            <h2 className='title-rim'>Select Rim</h2>
+      </div>
+        <div className='section-prev-custom'>
+          <div className='title-customize'>
+            <img className='img-title-makeIt' src="./image/title-makeIt.png" alt="make it" />
+          </div>
+          <div className='contenedor-img-car'>
+            <img className='img-config' src="https://configuratormedia.lamborghini.com/renderservice/media/fast/H4sIAAAAAAAAAC2Vz8rtNBDAj_eiXgS9qIj4Ah-C4KT_28WHXHGnC0Fx4V18TP41OUlz2iRtz-nax_GN3PgA15UbN4JzLkKYX2cymUwm0_b3v07vrvH0_PXrb7__94vPxvGP7tnpdJ1Pp9M7ZH_2-tvf__78n_de_Pzn_-b3fju9m9fIL--Qy6_PPrATjgrOsxo_fPPmzcvT-2Q-PT-dXhJezHen00cfrdHPGHH6WsTLHD7-siuHQvd11fSsUyUbpOBcyKGUZdkPtRRMN7VmjfiE9nuJ3oMQoYA6V6VQDEpWVlDUXy3Md9C0Xw0DIK6KISC_tIYRYjGTpmR3GEJkcweozToWgOPUCZqzvF41oOOFqgmpOgh-XFQLOM9jIi3qM61Oqr0qwqiQIiffpDMhNnEl3JgjY8biSpFXOewUeU3YJ8ADj4Ok1AtJ9fZ5ongcTakmQj4zTtgK74BT3hqBK1NPDLjnnA7DL3uRCuAZuxut2-WQSTtwMiAw3KWM3YIgDJamI8TlzEBchGB3vPWY0WZSZm7XkiDtTlrklI2I813uWykZSMSStpKoyikQYpVKwkr5SmUrvRHcSJGkpkTJ8RJpSsm9Qw_KmiZnglsGTvDdVBDWpdegJhtiB2o23bUEjZJpB5oLyk0Lke7SUhparHECrdaWaqw9tn4kpEHUoKPT5xZ04qspQGdUFGBLHV2l3i3uoN8WWh_mrTyPNH0kTfUb5VTtLYwq9KOB0dwLNzrDbgdhY2fCdMm5hzHoemUwRuxzScibuGtbgzWMaR4LB0Z6Wm1UaigXY5FROxo7tSGAcTggeaS9V1ewIa1MEHIdarAx1b4Fh4q5nRBdL8DxjVGFnD_YjRAXtpJLTAWW4BI2gozJViMjrI4K4lbjXAXuMO1Mc8fW7gV4FI0rCU7zDjzfKT8v5mVh4BV65gmhCD14rTdqUm-xp5A-Uq8iYaPOA59UHxX4vFZGgl-nzgjwe-wpyoQyUJoTplb1MMmkQweTxmEho5EBYbKukgNM58PSQSa39kcDVFJpK5giHzyDgFu3lBBU6ncNF5nD0sKM0zgTqAVFJORhcDDHpOMA89bSgGVFpBwiypaqH_Fi-0BYhzNCFBcVC0KiHozSdVSbqGzfakIsxh6i1tQMMWWmyC9t1cIhIYa1JqylS5B47BMSMp0jKYsZkk6lKyCNbpgbSEbSZy1ZHtiVoKp5IpiariPZrZEb4WCeFrvc0OmTV12sIE2oN3IJ2PmeYPr7XIjpHjn5mq4qpZk1B6QsO14SdHleCFdtIG0Xfd-ALuBKOGJ5u2NlQUB2W21LyPM4cEbwvKghR93IDnKaA72cmU7qM-RNLVsFeffFFiDf5oL6Mt-Okm5n464gz-2SlnqGLW_DcYWdjTRgbwoasGNUewW7iNRRuw1dYrCnVI8cbrumAYeeWCZYVR-BkGl8o8JmI310VMiPP-VV2svTDzjxSxyNDfZhtzKbx2Io2YNRdjT5sWA9e0A_G3zMcVUPAicV8fHVLz-W3z29eiqfGKseMND_JdtLSI_f4c3fF376nFz-A8pwpA-3BgAA.jpg?viewPort=1280,576,fill" alt="make it" />
+          </div>
+        </div>
+        <div className='div-color'>
+          <h2 className='title-color'>Select color</h2>
+            <div className='section-color'>
+              <img className='color-1' src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQcAAADACAMAAAA+71YtAAAAA1BMVEXjABuGu5rPAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+BsYAAAF7hZJ0AAAAAElFTkSuQmCC'></img>
+            </div>
+        </div>
+      
+    </div>
+
+  )
+}
---
+++ src/pages/Custome/Custome.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Customize from '../../components/Customize/Customize'
+
+export default function Custome() {
+  return (
+    <div>
+    <Customize/>
+    </div>
+  )
+}
