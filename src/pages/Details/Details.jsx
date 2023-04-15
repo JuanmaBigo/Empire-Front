d8cea47763898586d1034805f3d6e345cc06cc27
RominaGaite <gaiteromina@gmaul.com>
2023-04-15 20:53:01 -0300

    .

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -36,7 +36,7 @@ const Details = () => {
                 <source src={data.video} type="video/mp4" />
               </video>
               <h1 className='detailsTitle'>{data.name}</h1>
-              <Anchor to="#" className="Detailsbtn"><p> Build your {data.name} </p></Anchor>
+              <Anchor to="customize" className="lastBtn"> Build your {data.name} </Anchor>
             </div>
             <div className='speedDetails'>
               <div className='carDetails'>
@@ -82,8 +82,6 @@ const Details = () => {
           <div className='Btn'>
             <Anchor to="customize" className="lastBtn"> Build your {data.name} </Anchor>
           </div>
-
-
         </div>
         : null}
     </>
